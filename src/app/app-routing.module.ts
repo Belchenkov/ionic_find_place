83821234eff68c2b212ab8332b34a4aa36e304e2
Aleksey Belchenkov <u608110@gmail.com>
2020-06-02 14:18:16 +0300

    Create App Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,14 @@ const routes: Routes = [
     path: '',
     redirectTo: '/',
     pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
   }
 ];
 
