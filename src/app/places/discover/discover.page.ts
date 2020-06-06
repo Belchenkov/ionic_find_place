2b00f79b2550ba3bc8fc4b0e630f1b730fae4173
Aleksey Belchenkov <u608110@gmail.com>
2020-06-06 11:05:15 +0300

    Outputting Places

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PlacesService } from "../places.service";
+import { Place } from "../place.model";
+
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  loadedPlaces: Place[];
 
-  constructor() { }
+  constructor(
+      private placesService: PlacesService
+  ) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placesService.places;
   }
 
 }
