bf83693fa4621e014b3ca8c1d4ef9f9f5aad8fb2
Aleksey Belchenkov <u608110@gmail.com>
2020-06-26 14:13:33 +0300

    Virtual Scrolling

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -11,6 +11,7 @@ import { Place } from "../place.model";
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(
       private placesService: PlacesService,
@@ -19,5 +20,6 @@ export class DiscoverPage implements OnInit {
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 }
