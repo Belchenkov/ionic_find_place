617cb9f832b28d593cb016e727d41532ef6ef3b7
Aleksey Belchenkov <u608110@gmail.com>
2020-07-13 10:52:07 +0300

    Switch Between Auth Modes

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -12,6 +12,7 @@ import { AuthService } from "./auth.service";
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
       private authService: AuthService,
@@ -38,6 +39,10 @@ export class AuthPage implements OnInit {
         this.router.navigate(['/places', 'tabs', 'discover']);
       }, 3000);
     });
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
   onSubmit(form: NgForm) {
