b793e4de9146d686dd4de0e50e12407b6415ae47
Aleksey Belchenkov <u608110@gmail.com>
2020-07-21 12:12:58 +0300

    Random Date

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -66,7 +66,8 @@ export class PlaceDetailPage implements OnInit {
     this.modalCtrl.create({
       component: CreateBookingComponent,
       componentProps: {
-        selectedPlace: this.place
+        selectedPlace: this.place,
+        selectedMode: mode
       }
     }).then(modalEl => {
       modalEl.present();
