74a268ab76e39a3e887014849a1a3c2e0f751bb0
Aleksey Belchenkov <u608110@gmail.com>
2020-08-19 13:13:02 +0300

    Deleting Bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -72,13 +72,16 @@ export class BookingService {
     }
 
     cancelBooking(bookingId: string) {
-        return this.bookings.pipe(
-            take(1),
-            delay(2000),
-            tap(bookings => {
-                this._bookings.next(bookings.filter(b => b.id !== bookingId));
-            })
-        );
+        return this.http.delete(`${this.apiUrl}/bookings/${bookingId}.json`)
+            .pipe(
+                switchMap(() => {
+                    return this.bookings;
+                }),
+                take(1),
+                tap(bookings => {
+                    this._bookings.next(bookings.filter(b => b.id !== bookingId));
+                })
+            );
     }
 
     fetchingBookings() {
