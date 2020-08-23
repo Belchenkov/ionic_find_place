9eaaf92f10b51c0d76e9e0cdea74fadb6a87b2c2
Aleksey Belchenkov <u608110@gmail.com>
2020-08-23 16:38:39 +0300

    Rendering a Map

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,7 +1,12 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
 import { ModalController } from "@ionic/angular";
-import {rejects} from "assert";
-import {log} from "util";
 
 @Component({
   selector: 'app-map-modal',
@@ -9,9 +14,11 @@ import {log} from "util";
   styleUrls: ['./map-modal.component.scss'],
 })
 export class MapModalComponent implements OnInit, AfterViewInit {
+  @ViewChild('map', { static: false }) mapElementRef: ElementRef;
 
   constructor(
-      private modalCtrl: ModalController
+      private modalCtrl: ModalController,
+      private renderer: Renderer2
   ) { }
 
   ngOnInit() {}
@@ -19,7 +26,23 @@ export class MapModalComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.getGoogleMaps()
         .then(googleMaps => {
+          const mapEl = this.mapElementRef.nativeElement;
+          const map = new googleMaps.Map(mapEl, {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 16
+          });
 
+          googleMaps.event.addListenerOnce(map, 'idle', () => {
+            this.renderer.addClass(mapEl, 'visible');
+          });
+
+          map.addListener('click', event => {
+            const selectedCoords = {
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng()
+            };
+            this.modalCtrl.dismiss(selectedCoords);
+          });
         }).catch(err => console.log(err));
   }
 
@@ -37,7 +60,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
 
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyB3P1U1KZYcRvql3VMXl2-g5GQlaK6AVlQ';
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA_8n0BN3aBi6jASxQSEKT9zqgswsT4kxE';
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
