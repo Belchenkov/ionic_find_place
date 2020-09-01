e936066365fa21c7b511efcf7e398aba1a50ab09
Aleksey Belchenkov <u608110@gmail.com>
2020-09-01 17:30:12 +0300

    Generate Image Picker Component

---
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-picker',
+  templateUrl: './image-picker.component.html',
+  styleUrls: ['./image-picker.component.scss'],
+})
+export class ImagePickerComponent implements OnInit {
+  selectedImage: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onPickImage() {
+
+  }
+
+}
